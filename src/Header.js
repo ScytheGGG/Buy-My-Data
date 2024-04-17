b22984aedf14d2447b038f7544e0a6a4f7e8850a
Karl Tammehoid <tammehoidkarl@gmail.com>
2024-04-18 01:11:43 +0300

    Navbar, logo & favicon

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Header() {
+        const headerStyle = {
+                fontFamily: 'Inter, sans-serif', // Specify Inter as the primary font
+        };
+
+        return (
+                <nav className="Navbar" style={headerStyle}>
+                        <img src="./photos/BuyMyData.png" alt="Buy My Data logo" className="Navbar--logo" width="8%"></img>
+                        <div className="Navbar--texts">
+                                <p className="Navbar--text">Home</p>
+                                <p className="Navbar--text">About</p> 
+                                <a href="" className="Navbar--text Navbar--login">Login</a>
+                        </div>
+                </nav>
+        )
+} 
