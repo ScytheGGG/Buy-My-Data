33c6f7fe1cfbbbe316fe6aa97d00d299f5b17586
Karl Tammehoid <tammehoidkarl@gmail.com>
2024-04-18 19:43:18 +0300

    Testing artwork

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react"
 
 // website elements
 import Header from "./Header"
+import Mainpage from "./Mainpage"
 
 // the app itself
 export default function App() {
   return (
     <div>
       <Header />
+      <Mainpage />
     </div>
   )
 }
---
+++ src/Mainpage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Mainpage() {
+        const MainpageStyle = {
+                fontFamily: 'Inter, sans-serif', // Specify Inter as the primary font
+        };
+
+        return (
+                <nav className="Mainpage" style={MainpageStyle}>
+                        <img src="./photos/Artwork-2.jpg" alt="Artwork 1" width="35%"></img>
+                        <div className="Mainpage--introduction">
+                                <p className="Mainpage--title" width="20%">Control & decide what to do with your own data.</p>
+                                <p className="Mainpage-- description"> Buy My data was made to take control, unlock rewards, and explore the new frontier of data ownership.
+                                </p>
+                        </div>
+                </nav>
+        )
+} 
