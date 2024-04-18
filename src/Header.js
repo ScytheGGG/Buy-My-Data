634a25843fccf0ab10050b1fb3a304dfe842ac38
Karl Tammehoid <tammehoidkarl@gmail.com>
2024-04-18 17:04:15 +0300

    Navbar tweaks

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,8 @@ export default function Header() {
                         <img src="./photos/BuyMyData.png" alt="Buy My Data logo" className="Navbar--logo" width="8%"></img>
                         <div className="Navbar--texts">
                                 <p className="Navbar--text">Home</p>
-                                <p className="Navbar--text">About</p> 
+                                <p className="Navbar--text">Services</p>
+                                <p className="Navbar--text Navbar--gap">About us</p> 
                                 <a href="/signin" className="Navbar--text Navbar--login">Login</a>
                         </div>
                 </nav>
